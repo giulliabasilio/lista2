bb8a61f826c08d70b7bd43ede3490d4b9b23b6cf
giulliabasilio <giulliabasilio@gmail.com>
2025-05-26 16:59:24 -0300

    Add files via upload

---
+++ lista2ex5/lista2ex5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor da base do retângulo:");
+            double baseRetangulo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura do retângulo:");
+            double altura = double.Parse(Console.ReadLine());
+
+            double area = baseRetangulo * altura;
+
+            Console.WriteLine("Área do retângulo: " + area);
+
+            if (area > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+    }
+}
