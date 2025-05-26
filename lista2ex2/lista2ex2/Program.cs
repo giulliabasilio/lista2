0303dfc69c946c1d1d1f295e164b3ba656a83860
giulliabasilio <giulliabasilio@gmail.com>
2025-05-26 16:47:28 -0300

    Add files via upload

---
+++ lista2ex2/lista2ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            if (numero1 == numero2)
+            {
+                Console.WriteLine("Os números são idênticos.");
+            }
+            else
+            {
+                int maior = (numero1 > numero2) ? numero1 : numero2;
+                Console.WriteLine("O maior número é: " + maior);
+            }
+        }
+    }
+}
