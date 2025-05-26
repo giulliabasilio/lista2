2e724de92e7b8939cc2cb7eea7d061470fe1e924
giulliabasilio <giulliabasilio@gmail.com>
2025-05-26 17:02:38 -0300

    Add files via upload

---
+++ lista2ex6/lista2ex6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o peso da pessoa (em kg):");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura da pessoa (em metros):");
+            double altura = double.Parse(Console.ReadLine());
+
+            double r = peso / (altura * altura);
+
+            Console.WriteLine("R (peso / altura²): " + r.ToString("F2")); 
+
+            if (r < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (r < 25)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+            else
+            {
+                Console.WriteLine("Acima do peso");
+            }
+
+        }
+    }
+}
