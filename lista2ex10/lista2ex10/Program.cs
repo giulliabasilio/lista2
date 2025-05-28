762292c40256b467d77fecb824cd91cd7b98d5a3
giulliabasilio <giulliabasilio@gmail.com>
2025-05-28 16:26:37 -0300

    Add files via upload

---
+++ lista2ex10/lista2ex10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota da P1:");
+            double p1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da P2:");
+            double p2 = double.Parse(Console.ReadLine());
+
+            double media = (p1 + 2 * p2) / 3;
+
+            Console.WriteLine($"Média: {media:F2}");
+
+            if (media >= 5)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+
+
+        }
+    }
+}
